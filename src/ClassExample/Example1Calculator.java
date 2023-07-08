1e1e6f2367ba009384fe8ce5698526ca3778c6cd
sonalade <sonalade2@gmail.com>
2023-07-08 22:07:53 +0530

    Example1Calculator

---
+++ src/ClassExample/Example1Calculator.java
@@ -0,0 +1,20 @@
+package ClassExample;
+import java.util.Scanner;
+public class Example1Calculator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to calculator");
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        int a=sc.nextInt();
+        System.out.println("Enter second number");
+        int b=sc.nextInt();
+
+        System.out.println("sum = "+(a+b));
+        System.out.println("sub = "+(a-b));
+        System.out.println("mul = "+(a*b));
+        System.out.println("div = "+(a/b));
+
+    }
+}
